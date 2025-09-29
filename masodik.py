2ddac8719dd02d35d0fda09e1920200251453035
Kiss Barnabás <b3kisbar@vasvari.org>
2025-09-29 14:07:33 +0200

    attila

---
+++ masodik.py
@@ -0,0 +1,8 @@
+old1 = int(input("Kérem a téglalap egyik oldalának a hosszát (cm): "))
+old2 = int(input("Kérem a téglalap másik oldalának a hosszát (cm): "))
+
+terulet = old1 * old2
+kerulet = 2 * (old1 + old2)
+
+print("A téglalap területe:", terulet, "cm2")
+print("A téglalap kerülete:", kerulet, "cm")
